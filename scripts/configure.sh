fb5561846f65e9b43a1f9015d169a0b5c4421b22
Radu Andries <admiral0@tuxfamily.org>
2012-09-30 21:49:15 +0200

    Solved ANNOYING bug

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+if [ ! -d /scratchbox/users/$USER/home/$USER/lpsmagic/build ]; then
+  mkdir /scratchbox/users/$USER/home/$USER/lpsmagic/build
+fi
 /scratchbox/login -d $HOME/lpsmagic/build cmake ..
--- scripts/mount.sh
+++ scripts/mount.sh
@@ -2,4 +2,7 @@
 
 sudo /scratchbox/sbin/sbox_ctl start
 sudo /scratchbox/sbin/sbox_sync
+if [ ! -d /scratchbox/users/$USER/home/$USER/lpsmagic ]; then
+  mkdir /scratchbox/users/$USER/home/$USER/lpsmagic
+fi
 sudo mount -o bind $HOME/projects/lpsmagic /scratchbox/users/$USER/home/$USER/lpsmagic
